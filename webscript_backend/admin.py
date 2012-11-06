ba359746b96856952ef4122ace071ef063e5e06a
Shaon <sbarman@L3CWZ5T.(none)>
2012-11-06 13:59:08 -0800

    added admin console for replays

--- webscript_backend/admin.py
+++ webscript_backend/admin.py
@@ -59,11 +59,47 @@ class ParameterAdmin(admin.ModelAdmin):
     ordering = ['id', 'name']
     search_fields = ('name', 'value')
 
+class InlineReplayEvent(admin.TabularInline):
+    model = models.ReplayEvent
+
+    date_hierarchy = 'creation_date'
+    fields = ('event_type', 'execution_order')
+    ordering = ['replay', 'execution_order']
+
 class ReplayAdmin(admin.ModelAdmin):
     date_hierarchy = 'creation_date'
+    inlines =  [InlineReplayEvent]
+    list_display = ('script__name', 'user', 'creation_date', 'description')
+    list_filter = ('user__username',)
+    search_fields = ('^script__name', 'description', '^user__username')
+    save_as = True
 
 class ReplayEventAdmin(admin.ModelAdmin):
+
+    def display_parameters(self, obj):
+        """
+        Summarize parameters for this event.
+        """
+        l = []
+        for param in obj.parameters.all():
+            if len(param.value) > 16:
+                l.append(u"{}={}...".format(param.name, param.value[:16]))
+            else:
+                l.append(u"{}={}".format(param.name, param.value))
+        return "; ".join(l)
+    display_parameters.short_description = "Parameters"
+
     date_hierarchy = 'creation_date'
+    inlines = [InlineParameter]
+    list_display = ('execution_order', 'event_type', 'display_parameters',
+                    'replay',)
+    list_filter = ('replay__script__name', 'replay__id', 
+                   'replay__script__user__username', 'event_type')
+    ordering = ['replay__script', 'execution_order']
+    search_fields = ('event_type', 'dom_pre_event_state',
+                     'dom_post_event_state', 'replay__script__name',
+                     'parameter__name', 'parameter__value',)
+    save_as = True
 
 admin.site.register(models.Script, ScriptAdmin)
 admin.site.register(models.Event, EventAdmin)
